6258dab60ee4e1b4e4d747c0a8a67f3d480ad44f
Martin Kadrev <martokadrev@gmail.com>
2021-08-02 12:51:42 +0300

    Storage is now MongoDB, configured to connect to a MongoDB service on K8

--- src/main.go
+++ src/main.go
@@ -1,22 +1,46 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
+	"fmt"
 	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	"strconv"
 	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	client        *mongo.Client
+	mongoURL      string
+	ctx           context.Context
+	notesDatabase *mongo.Database
+	collection    *mongo.Collection
+	err           error
 )
 
 func main() {
 
-	jsonPath := "../notes/notes.json"
-	notes := getNotes(jsonPath)
+	mongoURL = "mongodb://root:aVFZR1VjQlox@mongodb:27017"
+	client, err = mongo.NewClient(options.Client().ApplyURI(mongoURL))
+	checkError(err)
+
+	ctx, _ = context.WithTimeout(context.Background(), 3*time.Second)
+	err = client.Connect(ctx)
+	checkError(err)
+	defer client.Disconnect(ctx)
+
+	notesDatabase = client.Database("go-notes")
+	collection = notesDatabase.Collection("notes")
+
+	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+
+	fmt.Println("connected to nosql database:", mongoURL)
 
 	router := mux.NewRouter()
 
@@ -24,28 +48,24 @@ func main() {
 
 	// Request Handlers
 	router.Handle("/", fileServer)
-	router.HandleFunc("/new", notes.handleAddNote)
-	router.HandleFunc("/notes", notes.handleViewNote)
-	router.HandleFunc("/details/{id}", notes.handleEditNote)
-	router.HandleFunc("/editNoteData/{id}", notes.handleEditNoteData)
-	router.HandleFunc("/deleteNote/{id}", notes.handleDeleteNote)
+	router.HandleFunc("/new", handleAddNote)
+	router.HandleFunc("/notes", handleViewNote)
+	router.HandleFunc("/details/{id}", handleEditNote)
+	router.HandleFunc("/editNote/{id}", handleEditNoteData)
+	router.HandleFunc("/deleteNote/{id}", handleDeleteNote)
 
 	log.Println("Server Has Successfully Started at Port :8080...")
-	err := http.ListenAndServe(":8080", router)
+	err = http.ListenAndServe(":8080", router)
 	checkError(err)
 
 }
 
 type Note struct {
-	Data           string `json:"data"`
-	Author         string `json:"author"`
-	TimeCreated    string `json:"time_created"`
-	TimeLastEdited string `json:"time_last_edited"`
-}
-
-type notesCollection struct {
-	notes    map[int]*Note
-	fileName string
+	Id             primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Data           string             `json:"data,omitempty" bson:"data,omitempty"`
+	Author         string             `json:"author,omitempty" bson:"author,omitempty"`
+	TimeCreated    string             `json:"time_created,omitempty" bson:"time_created,omitempty"`
+	TimeLastEdited string             `json:"time_last_edited,omitempty" bson:"time_last_edited,omitempty"`
 }
 
 type noteInfo struct {
@@ -53,34 +73,7 @@ type noteInfo struct {
 	Data *Note
 }
 
-// Populate the in-memory map
-func getNotes(path string) *notesCollection {
-
-	var notesFromFiles *notesCollection = &notesCollection{}
-
-	_, err := os.Stat(path)
-	if os.IsNotExist(err) {
-		_, err := os.Create(path)
-		checkError(err)
-	}
-
-	file, err := ioutil.ReadFile(path)
-	checkError(err)
-
-	notesFromFiles.notes = make(map[int]*Note)
-	notesFromFiles.fileName = path
-
-	json.Unmarshal(file, &notesFromFiles.notes)
-
-	for note := range notesFromFiles.notes {
-
-		log.Println(notesFromFiles.notes[note])
-	}
-
-	return notesFromFiles
-}
-
-func (h *notesCollection) handleAddNote(w http.ResponseWriter, r *http.Request) {
+func handleAddNote(w http.ResponseWriter, r *http.Request) {
 
 	// Return the form if the request is get and add the element if the request is post
 	switch r.Method {
@@ -88,42 +81,29 @@ func (h *notesCollection) handleAddNote(w http.ResponseWriter, r *http.Request) 
 		{
 			author := r.FormValue("author")
 			data := r.FormValue("data")
+			currentTime := time.Now().Format("2006-01-02 3:4:5 PM")
+			var note Note = Note{
+				Author:         author,
+				Data:           data,
+				TimeCreated:    currentTime,
+				TimeLastEdited: currentTime,
+			}
+			ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+			_, err := collection.InsertOne(ctx, note)
+			checkError(err)
+			var notes []Note
+			ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+			cursor, err := collection.Find(ctx, bson.M{})
+			checkError(err)
+			defer cursor.Close(ctx)
 
-			currentTime := time.Now()
-			lastIndex := len(h.notes)
-			var added bool
-
-			for i := 0; i < lastIndex; i++ {
-
-				if _, exists := h.notes[i]; !exists {
-
-					h.notes[i] = &Note{
-						Author:         author,
-						Data:           data,
-						TimeCreated:    currentTime.Format("2006-01-02 3:4:5 PM"),
-						TimeLastEdited: currentTime.Format("2006-01-02 3:4:5 PM"),
-					}
-					added = true
-					break
-				}
+			for cursor.Next(ctx) {
+				var n Note
+				cursor.Decode(&n)
+				notes = append(notes, n)
 			}
 
-			if !added {
-				h.notes[lastIndex] = &Note{
-					Author:         author,
-					Data:           data,
-					TimeCreated:    currentTime.Format("2006-01-02 3:4:5 PM"),
-					TimeLastEdited: currentTime.Format("2006-01-02 3:4:5 PM"),
-				}
-			}
-
-			log.Println("Note added: ", *h.notes[lastIndex])
-
-			notesBytes, err := json.MarshalIndent(h.notes, "", " ")
-			checkError(err)
-
-			err = ioutil.WriteFile(h.fileName, notesBytes, 0644)
-			checkError(err)
+			log.Println(notes)
 
 			http.Redirect(w, r, "http://localhost:8080", http.StatusFound)
 		}
@@ -140,23 +120,41 @@ func (h *notesCollection) handleAddNote(w http.ResponseWriter, r *http.Request) 
 }
 
 // Return the html to view all notes
-func (h *notesCollection) handleViewNote(w http.ResponseWriter, r *http.Request) {
+func handleViewNote(w http.ResponseWriter, r *http.Request) {
 
 	templ, err := template.ParseFiles("../static/viewNotes.html")
 	checkError(err)
 
-	err = templ.Execute(w, h.notes)
+	var notes []Note
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	checkError(err)
+
+	cursor, err := collection.Find(ctx, bson.M{})
+	checkError(err)
+
+	for cursor.Next(ctx) {
+		var n Note
+		cursor.Decode(&n)
+		notes = append(notes, n)
+		log.Println(n)
+	}
+
+	err = templ.Execute(w, notes)
 	checkError(err)
 }
 
 // Return the html with the edit note form based on the id
-func (h *notesCollection) handleEditNote(w http.ResponseWriter, r *http.Request) {
+func handleEditNote(w http.ResponseWriter, r *http.Request) {
 
 	vars := mux.Vars(r)
-	note, err := strconv.Atoi(vars["id"])
+	id := vars["id"]
+	idPrimitive, err := primitive.ObjectIDFromHex(id)
 	checkError(err)
 
-	var n noteInfo = noteInfo{Note: note, Data: h.notes[note]}
+	var n Note
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	cursor := collection.FindOne(ctx, bson.M{"_id": idPrimitive})
+	cursor.Decode(&n)
 
 	templ, err := template.ParseFiles("../static/editNote.html")
 	checkError(err)
@@ -168,45 +166,41 @@ func (h *notesCollection) handleEditNote(w http.ResponseWriter, r *http.Request)
 
 // Temporary function - will implement the algorithm
 // to handleEditNote - switch on the type of request (PUT/GET)
-func (h *notesCollection) handleEditNoteData(w http.ResponseWriter, r *http.Request) {
+func handleEditNoteData(w http.ResponseWriter, r *http.Request) {
 
 	vars := mux.Vars(r)
-	note, err := strconv.Atoi(vars["id"])
+	id := vars["id"]
+	idPrimitive, err := primitive.ObjectIDFromHex(id)
 	checkError(err)
 
 	author := r.FormValue("author")
 	data := r.FormValue("data")
-	currentTime := time.Now()
+	currentTime := time.Now().Format("2006-01-02 3:4:5 PM")
 
-	// Edit the in-memory data structure entry
-	h.notes[note].Author = author
-	h.notes[note].Data = data
-	h.notes[note].TimeLastEdited = currentTime.Format("2006-01-02 3:4:5 PM")
-
-	dat, err := json.MarshalIndent(h.notes, "", " ")
-	checkError(err)
-	err = ioutil.WriteFile(h.fileName, dat, 0644)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	_, err = collection.UpdateOne(ctx, bson.M{"_id": idPrimitive},
+		bson.M{
+			"$set": bson.M{"author": author, "data": data, "time_last_edited": currentTime},
+		},
+	)
 	checkError(err)
 
-	http.Redirect(w, r, "http://localhost:8080/notes", http.StatusFound)
+	http.Redirect(w, r, "/notes", http.StatusFound)
 }
 
 // Delete the note from in-memory and the JSON file
-func (h *notesCollection) handleDeleteNote(w http.ResponseWriter, r *http.Request) {
+func handleDeleteNote(w http.ResponseWriter, r *http.Request) {
 
 	vars := mux.Vars(r)
-	note, err := strconv.Atoi(vars["id"])
+	id := vars["id"]
+	idPrimitive, err := primitive.ObjectIDFromHex(id)
 	checkError(err)
 
-	delete(h.notes, note)
-
-	dat, err := json.MarshalIndent(h.notes, "", " ")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	_, err = collection.DeleteOne(ctx, bson.M{"_id": idPrimitive})
 	checkError(err)
 
-	err = ioutil.WriteFile(h.fileName, dat, 0644)
-	checkError(err)
-
-	http.Redirect(w, r, "http://localhost:8080/notes", http.StatusFound)
+	http.Redirect(w, r, "/notes", http.StatusFound)
 }
 
 func checkError(err error) {
